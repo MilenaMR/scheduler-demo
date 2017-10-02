4b5cf2436368c16937e98e13ba5f4203bb9dd087
MilenaMR <30763392+MilenaMR@users.noreply.github.com>
2017-10-02 18:46:10 -0300

    Update main.c
    Código funcionando

--- src/main.c
+++ src/main.c
@@ -3,12 +3,48 @@
 int main() {
   int a[5];
   int n;
-
+  int x=0;
+  
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
+  for (int i=0; i<n; ) {
     /* Imprime resultado da n-esima iteracao do scheduler */
+    i++;
+    if( i % a[0] == 0){
+       printf ("0");
+       x=1;
+       }
+
+    if( i % a[1] == 0){
+        printf ("1");
+	x=1;
+	}
+
+    if( i % a[2] == 0){
+        printf ("2");
+	x=1;
+	}
+
+    if( i % a[3] == 0){
+        printf ("3");
+	x=1;
+	}	
+
+    if( i % a[4] == 0){
+        printf ("4");
+	x=1;
+	}  
+    
+    if(x == 0){
+       printf("-");
+       }
+   
+
+     printf("\n");
+   
+     x=0;
   }
+  
 
   return 0;
 }
